f39cfe776f5685d6cf806b6e38c89b0bf2698bd4
Fortunatus KIDJE <elfriedv16@gmail.com>
2024-11-28 09:41:56 +0100

    add user usecase

---
+++ domain/Entities/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Domain\Entities;
+
+use DateTimeImmutable;
+
+class User
+{
+    private string $username;
+
+    private string $password;
+
+    private string $email;
+
+    private bool $isAdmin;
+
+    private DateTimeImmutable $createdAt;
+
+    private DateTimeImmutable $updatedAt;
+
+    public function __construct() {}
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getIsAdmin(): bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(bool $isAdmin): self
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+}
---
+++ domain/UseCases/User/CreateUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Domain\UseCases\User;
+
+use Domain\Entities\User;
+
+class CreateUser
+{
+
+    public function action(User $user) 
+    {
+        
+    }
+}
